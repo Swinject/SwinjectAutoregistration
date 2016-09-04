eb3b0d863cf7b22ceaf6d6fe1b51f2dae71a57f2
Tomas Kohout <email@tomaskohout.cz>
2016-09-04 12:32:30 +0200

    Added name to ~> operators

--- SwinjectAutoregistration/Classes/Operators.swift
+++ SwinjectAutoregistration/Classes/Operators.swift
@@ -37,6 +37,20 @@ public func ~> <Service>(r: ResolverType, service: Service.Type) -> Service {
     return r.resolve(service)!
 }
 
+/** Binary operator ~> equivalent to `r.resolve(Service.Type, name: "ServiceName")!`
+ 
+ Usage: `SomeClass(dependencyA: r ~> (DependencyA.self, name: "ServiceName"))`
+ - Parameters:
+ - r: Resolver
+ - service: Type of service to resolve.
+ 
+ - Returns: The resolved service type instance.
+ - Important: Fails on unresolvable service.
+ */
+public func ~> <Service>(r: ResolverType, o: (service: Service.Type, name: String)) -> Service {
+    return r.resolve(o.service, name: o.name)!
+}
+
 /** Binary operator ~> equivalent to `r.resolve(Service.Type, argument: Arg1)!`
  
  Usage: `SomeClass(dependencyA: r ~> (DependencyA.self, argument: arg))`
@@ -52,6 +66,21 @@ public func ~> <Service, Arg1>(r: ResolverType, o: (service: Service.Type, argum
     return r.resolve(o.service, argument: o.argument)!
 }
 
+/** Binary operator ~> equivalent to `r.resolve(Service.Type, name: String, argument: Arg1)!`
+ 
+ Usage: `SomeClass(dependencyA: r ~> (DependencyA.self, name: "ServiceName", argument: arg))`
+ - Parameters:
+ - r: Resolver
+ - o.service: Type of service to resolve.
+ - o.argument: Argument to pass
+ 
+ - Returns: The resolved service type instance.
+ - Important: Fails on unresolvable service.
+ */
+public func ~> <Service, Arg1>(r: ResolverType, o: (service: Service.Type, name: String, argument: Arg1) ) -> Service {
+    return r.resolve(o.service, name: o.name, argument: o.argument)!
+}
+
 /** Binary operator ~> equivalent to `r.resolve(Service.Type, arguments: (Arg1, Arg2))!`
  
  Usage: `SomeClass(dependencyA: r ~> (DependencyA.self, arguments: (arg1, arg2)))`
@@ -65,4 +94,51 @@ public func ~> <Service, Arg1>(r: ResolverType, o: (service: Service.Type, argum
  */
 public func ~> <Service, Arg1, Arg2>(r: ResolverType, o: (Service.Type, arguments: (Arg1, Arg2)) ) -> Service {
     return r.resolve(o.0, arguments: o.arguments)!
+}
+
+
+/** Binary operator ~> equivalent to `r.resolve(Service.Type, name: "ServiceName", arguments: (Arg1, Arg2))!`
+ 
+ Usage: `SomeClass(dependencyA: r ~> (DependencyA.self, name: "ServiceName", arguments: (arg1, arg2)))`
+ - Parameters:
+ - r: Resolver
+ - o.service: Type of service to resolve.
+ - o.arguments: Arguments to pass
+ 
+ - Returns: The resolved service type instance.
+ - Important: Fails on unresolvable service.
+ */
+public func ~> <Service, Arg1, Arg2>(r: ResolverType, o: (Service.Type, name: String, arguments: (Arg1, Arg2)) ) -> Service {
+    return r.resolve(o.0, name: o.name, arguments: o.arguments)!
+}
+
+
+/** Binary operator ~> equivalent to `r.resolve(Service.Type, arguments: (Arg1, Arg2, Arg3))!`
+ 
+ Usage: `SomeClass(dependencyA: r ~> (DependencyA.self, arguments: (arg1, arg2, arg3)))`
+ - Parameters:
+ - r: Resolver
+ - o.service: Type of service to resolve.
+ - o.arguments: Arguments to pass
+ 
+ - Returns: The resolved service type instance.
+ - Important: Fails on unresolvable service.
+ */
+public func ~> <Service, Arg1, Arg2, Arg3>(r: ResolverType, o: (Service.Type, arguments: (Arg1, Arg2, Arg3)) ) -> Service {
+    return r.resolve(o.0, arguments: o.arguments)!
+}
+
+/** Binary operator ~> equivalent to `r.resolve(Service.Type, name: "ServiceName", arguments: (Arg1, Arg2, Arg3))!`
+ 
+ Usage: `SomeClass(dependencyA: r ~> (DependencyA.self, name: "ServiceName", arguments: (arg1, arg2, arg3)))`
+ - Parameters:
+ - r: Resolver
+ - o.service: Type of service to resolve.
+ - o.arguments: Arguments to pass
+ 
+ - Returns: The resolved service type instance.
+ - Important: Fails on unresolvable service.
+ */
+public func ~> <Service, Arg1, Arg2, Arg3>(r: ResolverType, o: (Service.Type, name: String, arguments: (Arg1, Arg2, Arg3)) ) -> Service {
+    return r.resolve(o.0, name: o.name, arguments: o.arguments)!
 }
