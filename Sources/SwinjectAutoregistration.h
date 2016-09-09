0ddfe45b1c0edd80dee37a6a4d6a21f5f13bec60
Jakub Vaňo <me@jakubvano.com>
2016-09-09 20:45:20 +0200

    Setup OSX target

--- Sources/SwinjectAutoregistration.h
+++ Sources/SwinjectAutoregistration.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Swinject Contributors. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwinjectAutoregistration-iOS.
 FOUNDATION_EXPORT double SwinjectAutoregistration_iOSVersionNumber;
