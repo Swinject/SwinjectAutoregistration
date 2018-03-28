34c74359cefe2a75e50dab99b3f0a018c2bb458e
Jakub Vaňo <me@jakubvano.com>
2018-03-28 09:19:04 +0200

    Update tests for resolving optional dependencies

--- Tests/SwinjectAutoregistrationTests/AutoregistrationSpec.swift
+++ Tests/SwinjectAutoregistrationTests/AutoregistrationSpec.swift
@@ -195,6 +195,18 @@ class AutoregistrationSpec: QuickSpec {
                 let service = container.resolve(Service4.self, arguments: DependencyC(), DependencyD(), DependencyB())
                 expect(service).notTo(beNil())
             }
+
+            it("registers with service with optional dependencies") {
+                container.autoregister(OptionalService.self, initializer: OptionalService.init)
+                let service = container.resolve(OptionalService.self)
+                expect(service).notTo(beNil())
+            }
+
+            it("registers with service with unwrapped dependencies") {
+                container.autoregister(UnwrappedService.self, initializer: UnwrappedService.init)
+                let service = container.resolve(UnwrappedService.self)
+                expect(service).notTo(beNil())
+            }
             
             #if !os(Linux)
             it("throws assertion when same type arguments are passed") {
@@ -202,25 +214,11 @@ class AutoregistrationSpec: QuickSpec {
                     container.autoregister(SameArgumentsService.self, arguments: String.self, Int.self, String.self, initializer: SameArgumentsService.init)
                 }.to(throwAssertion())
             }
-            
+
             it("throws assertion when trying to resolve service with too many dependencies") {
                 expect { () -> Void in
                     container.autoregister(Service21.self, initializer: Service21.init)
                     _ = container.resolve(Service21.self)
-                }.to(throwAssertion())
-            }
-            
-            it("throws assertion when trying to resolve service with optional dependency") {
-                expect { () -> Void in
-                    container.autoregister(OptionalService.self, initializer: OptionalService.init)
-                    _ = container.resolve(OptionalService.self)
-                }.to(throwAssertion())
-            }
-            
-            it("throws assertion when trying to resolve service with unwrapped dependency") {
-                expect { () -> Void in
-                    container.autoregister(UnwrappedService.self, initializer: UnwrappedService.init)
-                    _ = container.resolve(UnwrappedService.self)
                 }.to(throwAssertion())
             }
             #endif
