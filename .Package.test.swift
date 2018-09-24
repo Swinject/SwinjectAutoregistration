2d39b2408b7c58d0aaa09a3682530908d62c991f
Jakub Vano <jakub.vano@cleverlance.com>
2018-09-24 13:28:56 +0200

    Update dependencies for package manager

--- .Package.test.swift
+++ .Package.test.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SwinjectAutoregistration",
     dependencies: [
-        .Package(url: "https://github.com/Swinject/Swinject.git", majorVersion: 2),
+        .Package(url: "https://github.com/Swinject/Swinject.git", majorVersion: 2, minor: 5),
         .Package(url: "https://github.com/Quick/Quick", majorVersion: 1),
-        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minor: 1),
+        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 7),
     ]
 )
