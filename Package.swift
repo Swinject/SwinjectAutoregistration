455744fd501427e4dfa74abe9ff1612e0d6c8c6f
Kristoffer Hermansen <kth@vertica.dk>
2024-04-25 13:47:24 +0200

    Updated to version 2.8.7

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 
     ],
     dependencies: [
-        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.4")
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.7")
     ],
     targets: [
         .target(
